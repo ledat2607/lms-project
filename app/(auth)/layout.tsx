67901ec3c12c51a2b0387b3252a225fdaa0c5eb4
LEDAT\ledat <ledat260701@gmail.com>
2025-07-08 15:51:13 +0700

    theme

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from "@/components/themeToggle";
+
 export default function AuthLayout({
   children,
 }: {
@@ -5,6 +7,7 @@ export default function AuthLayout({
 }) {
   return (
     <div className="relative flex min-h-svh w-full flex-col items-center justify-center">
+      <ModeToggle />
       <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
     </div>
   );
