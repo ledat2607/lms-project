a271852018fece5f8c19a705f71709ee0afee47d
LEDAT\ledat <ledat260701@gmail.com>
2025-07-13 08:20:03 +0700

    Github login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,42 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { GithubIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { GithubIcon, Loader } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
 export default function LoginPage() {
+  const [githubIsPending, startGithubTransition] = useTransition();
+
+  //Login function for GitHub
+  async function loginWithGithub() {
+    startGithubTransition(async () => {
+      try {
+        await authClient.signIn.social({
+          provider: "github",
+          callbackURL: "/",
+          fetchOptions: {
+            onSuccess: () => {
+              toast.success("Redirecting...");
+            },
+            onError: () => {
+              toast.error(`Login failed: Internal error`);
+            },
+          },
+        });
+      } catch (error) {
+        toast.error("Failed to login with GitHub");
+      }
+    });
+  }
   return (
     <Card className="flex flex-col gap-4">
       <CardHeader className="text-3xl font-bold dark:text-gray-200">
@@ -14,9 +46,23 @@ export default function LoginPage() {
         Please enter your credentials to continue.
       </CardDescription>
       <CardContent>
-        <Button variant={"default"} className="w-full">
-          <GithubIcon className="size-4" />
-          Login with Github
+        <Button
+          disabled={githubIsPending}
+          onClick={loginWithGithub}
+          variant={"default"}
+          className="w-full"
+        >
+          {githubIsPending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+              <span>Loading....</span>
+            </>
+          ) : (
+            <>
+              <GithubIcon className="size-4" />
+              Login with Github
+            </>
+          )}
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border mt-3">
           <span className="relative z-10 bg-card dark:bg-gray-700 dark:text-white px-2">
@@ -33,7 +79,7 @@ export default function LoginPage() {
               className="dark:bg-gray-700 dark:text-white"
             />
           </div>
-          <Button variant={'default'}>Coutinue with Email</Button>
+          <Button variant={"default"}>Coutinue with Email</Button>
         </div>
       </CardContent>
     </Card>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <div className="text-2xl">Home page</div>;
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { ModeToggle } from "@/components/themeToggle";
+import { Button } from "@/components/ui/button";
+
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  return (
+    <div className="p-12">
+      <h1 className="text-2xl">Home page</h1>
+      <ModeToggle />
+      {session ? (
+        <>
+          <p>{session.user.email}</p>
+        </>
+      ) : (
+        <>
+          <Button>Logout</Button>
+        </>
+      )}
+    </div>
+  );
 }
