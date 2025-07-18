9d38097fdd90b2a33271399c2cd9931a5b202d43
LEDAT\ledat <ledat260701@gmail.com>
2025-07-18 14:59:54 +0700

    Landing page

--- app/(public)/_components/SwitchMode.tsx
+++ app/(public)/_components/SwitchMode.tsx
@@ -19,26 +19,26 @@ export default function SwitchMode() {
 
   return (
     <div>
-      <div className="relative inline-grid h-6 grid-cols-[1fr_1fr] items-center text-sm font-medium">
+      <div className="relative inline-grid h-9 grid-cols-[1fr_1fr] items-center text-sm font-medium">
         <Switch
-          onClick={toggleTheme}
           id={id}
+          onClick={toggleTheme}
           checked={checked}
           onCheckedChange={setChecked}
-          className="peer data-[state=unchecked]:bg-input/50 absolute inset-0 h-[inherit] w-auto [&_span]:z-10 [&_span]:h-full [&_span]:w-1/2 [&_span]:transition-transform [&_span]:duration-300 [&_span]:ease-[cubic-bezier(0.16,1,0.3,1)] [&_span]:data-[state=checked]:translate-x-full [&_span]:data-[state=checked]:rtl:-translate-x-full"
+          className="peer data-[state=checked]:bg-primary/80 data-[state=unchecked]:bg-primary/80 absolute inset-0 h-[inherit] w-auto [&_span]:h-full [&_span]:w-1/2 [&_span]:transition-transform [&_span]:duration-300 [&_span]:ease-[cubic-bezier(0.16,1,0.3,1)] [&_span]:data-[state=checked]:translate-x-full [&_span]:data-[state=checked]:rtl:-translate-x-full"
         />
-        <span className="pointer-events-none relative ms-0.5 flex min-w-8 items-center justify-center text-center transition-all  duration-600 ease-[cubic-bezier(0.16,1,0.3,1)] peer-data-[state=checked]:invisible peer-data-[state=unchecked]:translate-x-full peer-data-[state=unchecked]:rtl:-translate-x-full">
+        <span className="peer-data-[state=checked]:text-muted-foreground/70 pointer-events-none relative ms-0.5 flex min-w-8 items-center justify-center text-center">
           <MoonIcon
             size={16}
             aria-hidden="true"
-            className="text-black dark:text-white"
+            className=" text-gray-500 dark:text-white"
           />
         </span>
-        <span className="peer-data-[state=checked]:text-background pointer-events-none relative me-0.5 flex min-w-8 items-center justify-center text-center transition-transform duration-300 ease-[cubic-bezier(0.16,1,0.3,1)] peer-data-[state=checked]:-translate-x-full peer-data-[state=unchecked]:invisible peer-data-[state=checked]:rtl:translate-x-full">
+        <span className="peer-data-[state=unchecked]:text-muted-foreground/70 pointer-events-none relative me-0.5 flex min-w-8 items-center justify-center text-center">
           <SunIcon
             size={16}
             aria-hidden="true"
-            className="text-white dark:text-black"
+            className="text-rose-800 dark:text-gray-500"
           />
         </span>
       </div>
