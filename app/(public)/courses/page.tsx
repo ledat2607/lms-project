c4df55e87ee38c0f16ade6be54d81ab90b0bcaaf
LEDAT\ledat <ledat260701@gmail.com>
2025-12-08 12:41:52 +0700

    Tets

--- app/(public)/courses/page.tsx
+++ app/(public)/courses/page.tsx
@@ -4,6 +4,7 @@ import PublicCoursePage from "./_components/PublicCoursePage";
 export default async function Page() {
   // gọi API / DB trên server
   const courses = await getAllCourses();
+  console.log(`check`, courses)
 
   // truyền courses xuống client
   return <PublicCoursePage initialCourses={courses} />;
